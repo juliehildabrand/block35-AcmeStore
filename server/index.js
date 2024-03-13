fb8dae5ca40f60af192bcc875485297b202dd057
Julie Hildabrand <jhildabrand257@gmail.com>
2024-03-13 17:00:02 -0400

    get routes done

--- server/index.js
+++ server/index.js
@@ -2,6 +2,40 @@ console.log('app working');
 
 //connect index.js to db.js
 const {client, createTables, createUser, fetchUsers, createProduct, fetchProducts, createFavorite, fetchFavorites, destroyFavorite} = require('./db');
+//bring in express
+const express = require('express');
+const app = express();
+
+//establish CRUD functionality
+//GET users
+app.get('/api/users', async(req, res, next)=> {
+  try {
+    res.send(await fetchUsers());
+  }
+  catch(error) {
+    next(error);
+  }
+});
+
+//GET products
+app.get('/api/products', async(req, res, next)=> {
+  try {
+    res.send(await fetchProducts());
+  }
+  catch(error) {
+    next(error);
+  }
+});
+
+//GET favorites
+app.get('/api/users/:id/favorites', async(req, res, next)=> {
+  try {
+    res.send(await fetchFavorites(req.params.id));
+  }
+  catch(error) {
+    next(error);
+  }
+});
 
 //create init function
 const init = async()=> {
@@ -10,6 +44,7 @@ const init = async()=> {
   console.log('connected to our database');
   await createTables();
   console.log('tables created');
+  //seed user and product information
   const [duff, saxophone, skateboard, homer, bart, lisa] = await Promise.all([
     createProduct({name: 'duff'}),
     createProduct({name: 'saxophone'}),
@@ -31,6 +66,15 @@ const init = async()=> {
   await destroyFavorite(bartDuffs);
 
   console.log(await fetchFavorites(bart.id));
+
+  //add listening port
+  const port = process.env.PORT || 3000;
+  app.listen(port, ()=> {
+    console.log(`listening on port ${port}`);
+    console.log(`curl localhost:${port}/api/users`);
+    console.log(`curl localhost:${port}/api/products`);
+    console.log(`curl localhost:${port}/api/users/${bart.id}/favorites`);
+  });
 };
 
 init();
