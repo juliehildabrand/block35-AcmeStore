4079668dcc34e9776c009d08ec07755f12faea10
Julie Hildabrand <jhildabrand257@gmail.com>
2024-03-13 16:37:00 -0400

    data layer done

--- server/db.js
+++ server/db.js
@@ -1,6 +1,10 @@
 //require postgres
 const pg = require('pg');
-const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_store_db');
+//require uuid
+const uuid = require('uuid');
+const bcrypt = require('bcrypt');
+//CREATE DATABASE acme_store_db;
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_store_gh_db');
 
 const createTables = async()=> {
   const SQL = `
@@ -19,14 +23,96 @@ const createTables = async()=> {
     CREATE TABLE favorites(
       id UUID PRIMARY KEY,
       user_id UUID REFERENCES users(id) NOT NULL,
-      product_id UUID REFERENCES products(id) NOT NULL
+      product_id UUID REFERENCES products(id) NOT NULL,
+      CONSTRAINT user_favorite_unique UNIQUE (user_id, product_id)
     );
   `;
   await client.query(SQL);
+};
+
+// createUser
+const createUser = async({username, password})=> {
+  const SQL = `
+    INSERT INTO users(id, username, password)
+    VALUES($1, $2, $3)
+    RETURNING *
+  `;
+  const response = await client.query(SQL, [uuid.v4(), username, await bcrypt.hash(password, 5)]);
+  return response.rows[0];
+};
+
+const fetchUsers = async()=> {
+  const SQL = `
+    SELECT *
+    FROM users
+  `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+createProduct = async({name})=> {
+  const SQL = `
+    INSERT INTO products(id, name)
+    VALUES ($1, $2)
+    RETURNING *
+  `;
+  const response = await client.query(SQL, [uuid.v4(), name])
+  return response.rows[0];
+};
+
+const fetchProducts = async()=> {
+  const SQL = `
+    SELECT *
+    FROM products
+  `;
+  const response = await client.query(SQL);
+  return response.rows;
+};
+
+const createFavorite = async({user_id, product_id}) => {
+  console.log(user_id);
+  const SQL = `
+    INSERT INTO favorites(id, user_id, product_id)
+    VALUES($1, $2, $3)
+    RETURNING *
+  `;
+  const response = await client.query(SQL, [uuid.v4(), user_id, product_id])
+  return response.rows[0];
+};
+
+const fetchFavorites = async(user_id)=> {
+  const SQL = `
+    SELECT *
+    FROM favorites
+    WHERE user_id = $1
+  `;
+  const response = await client.query(SQL, [user_id]);
+  return response.rows;
+};
+
+const destroyFavorite = async({user_id, id})=> {
+  const SQL = `
+    DELETE FROM favorites
+    WHERE id = $1 AND user_id = $2
+    RETURNING *
+  `;
+  const response = await client.query(SQL, [id, user_id]);
+  if(!response.rows.length){
+    const error = Error('no favorite found');
+    error.status = 500;
+    throw error;
+  }
 }
 
 //we must export anything created in db.js that we want to pass into index.js and utilize in that file
 module.exports = {
   client,
-  createTables
+  createTables,
+  createProduct,
+  createUser,
+  fetchUsers,
+  fetchProducts,
+  createFavorite,
+  fetchFavorites,
+  destroyFavorite
 };
--- server/index.js
+++ server/index.js
@@ -1,15 +1,36 @@
-//CREATE DATABASE acme_store_db;
 console.log('app working');
 
 //connect index.js to db.js
-const {client, createTables} = require('./db.js');
+const {client, createTables, createUser, fetchUsers, createProduct, fetchProducts, createFavorite, fetchFavorites, destroyFavorite} = require('./db');
 
 //create init function
 const init = async()=> {
   console.log('connecting to our database');
   await client.connect();
   console.log('connected to our database');
+  await createTables();
   console.log('tables created');
+  const [duff, saxophone, skateboard, homer, bart, lisa] = await Promise.all([
+    createProduct({name: 'duff'}),
+    createProduct({name: 'saxophone'}),
+    createProduct({name: 'skateboard'}),
+    createUser({ username: 'homer', password: 'doh'}),
+    createUser({ username: 'bart', password: 'eatmyshorts'}),
+    createUser({ username: 'lisa', password: 'donthaveacow'})
+  ]);
+  console.log(await fetchUsers());
+  console.log(await fetchProducts());
+
+  //create a favorite of bart
+  const [bartSkateboards, bartDuffs] = await Promise.all([
+    createFavorite({ user_id: bart.id, product_id: skateboard.id}),
+    createFavorite({ user_id: bart.id, product_id: duff.id}),
+  ]);
+  console.log(await fetchFavorites(bart.id));
+
+  await destroyFavorite(bartDuffs);
+
+  console.log(await fetchFavorites(bart.id));
 };
 
 init();
